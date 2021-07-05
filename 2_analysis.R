4389981da3836c1da1391a8394bb474ac516842d
9laurence <p.laurence@hotmail.com>
2021-07-05 15:38:42 -0300

    Commit4

--- 2_analysis.R
+++ 2_analysis.R
@@ -40,6 +40,38 @@ sink("results/BF_negativo_por_positivo.txt")
 corBF[2]/corBF[3]
 sink()
 
+## Correlacoes Junho
+# Dados correlacoes
+data <- df %>% filter(mes == 6) %>%
+  select(ocupacao_leitos, vacina_1d_cum) %>%
+  na.omit()
+
+# Correlacao de Pearson
+cor <- cor.test(data$ocupacao_leitos, data$vacina_1d_cum)
+
+sink("results/Correlacao_de_peason_Junho.txt")
+cor
+sink()
+
+# Correlacao Bayesiana comparando modelo de relação negativa com positiva
+corBF1 <-  correlationBF(data$ocupacao_leitos, data$vacina_1d_cum)
+corBF2 <- correlationBF(data$ocupacao_leitos, data$vacina_1d_cum,
+                        nullInterval = c(-1, 0))
+
+corBF <- c(corBF1, corBF2)
+
+sink("results/Correlacoes_bayesianas_BF_junho.txt")
+corBF
+sink()
+
+sink("results/BF_negativo_por_nulo_junho.txt")
+corBF[2]/corBF[1]
+sink()
+
+sink("results/BF_negativo_por_positivo_junho.txt")
+corBF[2]/corBF[3]
+sink()
+
 ## Correlacoes com dados da primeira dose atrasados em 21 dias
 # Dados correlacoes
 data <- df %>% filter(mes > 2) %>%
@@ -123,3 +155,35 @@ df %>% filter(mes > 2) %>%
 # Salvar grafico
 ggsave("figs/vacina_ocupacao.png", dpi = 200, units = "cm", width = 30, height = 15)
 
+# Grafico junho
+
+df %>% filter(mes == 6) %>% 
+  select(datahora, ocupacao_leitos, vacina_1d_cum) %>%
+  na.omit() %>%
+  mutate(ocupacao_leitos = scale(ocupacao_leitos),
+         vacina_1d_cum = scale(vacina_1d_cum)) %>%
+  pivot_longer(-datahora) %>% 
+  ggplot(aes(x = datahora, y = value, color = name)) +
+  geom_line(size = 1) +
+  # annotate("text", label = "Vacinaçao", size = 8, 
+  #          x = as_date("2021-06-15"), y = 2.25, color = "#FDE725FF")+
+  geom_label(aes(x = as_date("2021-06-26"), y = 0.5, label = "Número de pessoas vacinadas \n com a primeira dose"), 
+             size = 4, color = "black", fill = "#FDE725FF")+
+  geom_label(aes(x = as_date("2021-06-25"), y = -1.9, label = "Ocupação dos Leitos"), 
+             size = 4, color = "white", fill = "#440154FF")+
+  labs(x = "Data", y = "Valores", 
+       title = "Porcentagem da ocupação de leitos por COVID-19 e número de vacinação",
+       subtitle = "Valores entre 01 de junho de 2021 até 30 de junho de 2021 no estado de São Paulo.",
+       caption = "As variáveis estão estandardizadas para facilitar a compreensão. \n Dados do Governo de São Paulo.") +
+  scale_color_ordinal() +
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
+  scale_x_date(breaks = scales::pretty_breaks(n = 30), expand = c(0,0))+
+  cowplot::theme_cowplot() +
+  theme(panel.grid.major.y = element_line(colour = "grey", linetype = 3),
+        panel.grid.major.x = element_line(colour = "grey", linetype = 3),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.text.x = element_text(angle = 90),
+        legend.position = "none")
+
+ggsave("figs/vacina_ocupacao_junho.png", dpi = 200, units = "cm", width = 30, height = 15)
--- README.md
+++ README.md
@@ -13,6 +13,8 @@ A grande questão relacionada as vacinas de COVID-19 é se elas funcionam ou nã
 Mas ela funciona? Movido por essa questão, analisei os indicadores da pandemia no estado de São Paulo para apresentar o que está acontecendo. Relacionando os dados do número de pessoas vacinadas com a primeira dose e a porcentagem de ocupação de leitos por conta da COVID-19 foi possível encontrar alguns resultados. Dessa forma, conduzimos correlações, de Pearson e Bayesianas, para entender a relação entre as variáveis. A correlação de Pearson apontou para uma correlação inversamente proporcional e significativa. Já a correlação Bayesiana, demonstrou evidência decisiva para que a correlação seja negativa entre o número de pessoas vacinadas e a porcentagem de ocupação dos leitos por conta de COVID-19.
 
 ![grafico_vacinacao_ocupacao](figs/vacina_ocupacao.png)
+![grafico_vacinacao_ocupacao_junho](figs/vacina_ocupacao_junho.png)
+
 
 Embora os resultados sejam de caráter correlacional, podemos observar um padrão emergindo, e esse padrão é pautado por testes estatísticos. Dados dos próximos meses, enquanto a vacinação progride, poderão trazer ainda mais insights sobre o papel das vacinas em relação a ocupação dos leitos. Mas com os dados entre março e junho, é possível observar que enquanto mais pessoas recebiam a primeira dose, menor foi o número de leitos ocupados por conta de COVID-19.
 
